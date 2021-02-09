ae354ceae411a4eb693905fe9c390a95686f7c1e
lawrenceli79 <lawrenceli79@gmail.com>
2021-02-09 15:10:58 +0900

    v1

--- SCReplace_KubufInit.py
+++ SCReplace_KubufInit.py
@@ -9,8 +9,9 @@ if(len(sys.argv)<=1):
 
 strFileIn = sys.argv[1]
 
-varList = [
+varList = [ \
     'kubuf', \
+    'tmpkubuf', \
     'tempkubuf', \
     'kubuftemp', \
     'kubufN', \
@@ -19,9 +20,13 @@ varList = [
     'delkubuf', \
     'prekubuf', \
     'tkubuf', \
+    'kubufflag', \
+    'preKubuf', \
+    'e_kubuf', \
+    'PrevKubuf', \
 ]
 
-sizeofList = [
+sizeofList = [ \
     'KUBUF', \
     'XKUBUF', \
 ]
@@ -30,18 +35,38 @@ with open(strFileIn) as fin:
     for line in fin:
         lineOut = line
         bFound = False
-        m = re.search(r"(DRS)*memset\((&?)(\w+),\s*0,\s*sizeof\((\w+)\)\);", line)
+        m = re.search(r"(DRSmemset|memset|lmemset)\(\s*(&?)([^,]+),\s*0,\s*sizeof\((\w+)\)(\*\w+)?\s*\);", line)
         if(m):
             strRef = m.group(2)
             strVar = m.group(3)
+            
+            mVar = re.match(r"(\w+)", strVar)
+            bFullVar = mVar!=None
+
+            mKubuf = re.search(r"kubuf", strVar, re.IGNORECASE)
+            mDont = re.search(r"dcont", strVar, re.IGNORECASE)
             strSizeof = m.group(4)
-            if(strVar in varList and (strSizeof==strVar or strSizeof in sizeofList)):
-                nStart = m.start()
-                nEnd = m.end()
-                strBefore = line[:nStart]
-                strAfter = line[nEnd:]
-                strReplace = "KubufInit({0}{1},sizeof({2}));".format(strRef, strVar, strSizeof)
+            strSizeofMultiplier = m.group(5)
+            #if(strVar in varList and (strSizeof==strVar or strSizeof in sizeofList)):
+            #if(mKubuf and not mDont and ((strSizeof==strVar and bFullVar) or strSizeof in sizeofList)):
+            bMatch = False
+            if(strSizeof in sizeofList):
+                bMatch = True
+            if(mKubuf and not mDont and strSizeof==strVar and bFullVar ):
+                bMatch = True
+            if(bMatch):
+                strBefore = line[:m.start()]
+                strAfter = line[m.end():]
+
+                strstrSizeofMultiplier2 = ""
+                if(strSizeofMultiplier!=None):
+                    strstrSizeofMultiplier2 = strSizeofMultiplier
+                    strRef = ""
+                
+                strReplace = "KubufInit({0}{1},sizeof({2}){3});".format(strRef, strVar, strSizeof, strstrSizeofMultiplier2)
                 lineOut = strBefore + strReplace + strAfter
+                    
+
         print(lineOut, end='')
 
 
--- a.cpp
+++ a.cpp
@@ -1,4 +1,13 @@
-    DRSmemset(kubuf, 0, sizeof(kubuf));
+    DRSmemset(lpshohou->kubuf, 0, sizeof(kubuf));
+    DRSmemset(kubuf_dcont, 0, sizeof(kubuf));
     DRSmemset(&kubuf, 0, sizeof(KUBUF));
+    DRSmemset( &kubuf, 0, sizeof(kubuf) );
+    DRSmemset( &kubuf, 0, sizeof(kubuf)*MAX );
+    lmemset((LPSTR)tempkubuf,0,sizeof(KUBUF)*(kasan_num+1));
+    DRSmemset(preKubuf,0,sizeof(preKubuf));
+    memset(&pkubuf->kuvol, 0, sizeof(pkubuf->kuvol));
+    DRSmemset(&lpshohou->kubuf[ct], 0, sizeof(KUBUF));
+    lmemset((LPSTR)d,0,sizeof(KUBUF));
+    lmemset((LPSTR)&lpshohou->kubuf[j+1],0,sizeof(KUBUF));
 
     int a = 0;
